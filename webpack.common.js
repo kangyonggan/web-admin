676885d1b70abfcaac5d6700782a0382da0f24c9
kangyonggan <kangyonggan@gmail.com>
2019-09-26 22:28:55 +0800

    extract-text-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     module: {
         rules: [{
             test: /\.css$/,
-            include: path.resolve(__dirname, "src"),
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
+            include: path.resolve(__dirname, "src"),// 仅加载src目录下的资源
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",// 编译后用什么loader来提取css文件
+                use: "css-loader"// 指需要什么样的loader去编译文件,这里由于源文件是.css所以选择css-loader
+            })
         }, {
             test: /\.(png|svg|jpg|gif)$/,
-            include: path.resolve(__dirname, "src"),
+            include: path.resolve(__dirname, "src"),// 仅加载src目录下的资源
             loader: 'file-loader'
         }]
     },
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,14 +2,21 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = merge(common, {
     output: {
-        filename: 'main.[hash].js'
+        filename: '[name].[hash].js'
     },
     plugins: [
         // 清理dist目录
         new CleanWebpackPlugin(),
-        new UglifyJSPlugin()
+        // 清除未依赖的代码并压缩
+        new UglifyJSPlugin(),
+        // 抽离css样式
+        new ExtractTextPlugin({
+            filename: '[name].[hash].css',
+            allChunks: true
+        })
     ]
 });
